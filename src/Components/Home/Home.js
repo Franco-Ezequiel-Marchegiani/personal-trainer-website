dfa8cb2d2ee88392940e1df206794297613c39ca
Franco Ezequiel <eze.cavs@hotmail.com>
2022-09-09 12:17:50 -0300

    Setting structure of website

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,20 @@
+import logo from '../../logo.svg';
+
+function Home(){
+
+    return(
+        <section>
+            <header>
+                <div>
+                    <img src="" alt="" />
+                    <div>
+                        <button>Registrate</button>
+                        <button>Iniciar Sesión</button>
+                    </div>
+                </div>
+            </header>
+        </section>
+    )
+}
+
+export default Home;
